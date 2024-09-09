099534fe1c1e7a8281d9d7d06620278c9e83e552
Власник <@DESKTOP-N3BE1Q4>
2024-09-09 21:42:17 +0300

    Add project files.

---
+++ twoo/FileName.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Alone I'm on my own, at the party, yea\nAlone I'm on my own\nAlone I'm on my own at the party\nAlone I'm on my own\nWroten by J Bookout and Cris Waters" << endl;
+}
